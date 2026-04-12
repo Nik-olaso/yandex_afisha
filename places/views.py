1ba72e758539948f9e57eab36a64095da69fb31b
Nik-olaso <nikolaymalyshev001@gmail.com>
2026-04-12 10:29:38 +0300

    Звменил тестовые данные

--- places/views.py
+++ places/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Place
+from django.http import JsonResponse
 
-# Create your views here.
+
+def index(request):
+    """Главная страница, которая отдает все места из БД в формате GeoJSON."""
+    places = Place.objects.all()
+    features = []
+    for place in places:
+        features.append({
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.lng, place.lat]
+            },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": f"/places/{place.id}/",
+            }
+        })
+
+    places_geojson = {
+        "type": "FeatureCollection",
+        "features": features
+    }
+
+    return render(request, 'index.html', context={'places': places_geojson})
+
+
+def place_detail(request, place_id):
+    """Возвращает детальную информацию об одном месте в формате JSON."""
+    place = get_object_or_404(Place, id=place_id)
+
+    all_images = place.images.all().order_by('position')
+    image_urls = [img.image.url for img in all_images]
+
+    response_data = {
+        "title": place.title,
+        "imgs": image_urls,
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lat": place.lat,
+            "lng": place.lng,
+        }
+    }
+
+    return JsonResponse(
+        response_data,
+        json_dumps_params={'ensure_ascii': False, 'indent': 2}
+    )
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -19,12 +19,13 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from where_to_go import views
+from places import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.show_mainpage),
+    path('', views.index, name='index'),
+    path('places/<int:place_id>/', views.place_detail),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

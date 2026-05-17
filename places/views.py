230c36df8aace1a4240e974db0231b8895faf4e5
Nik-olaso <122684717+Nik-olaso@users.noreply.github.com>
2026-05-17 11:56:15 +0300

    Убрал зашитые ссылки, сократил количество запросов к БД и исправил .gitignore

--- places/views.py
+++ places/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Place
 from django.http import JsonResponse
+from django.urls import reverse
 
 
 def index(request):
@@ -16,7 +17,7 @@ def index(request):
             "properties": {
                 "title": place.title,
                 "placeId": place.id,
-                "detailsUrl": f"/places/{place.id}/",
+                "detailsUrl": reverse('place_detail', args=[place.id]), 
             }
         })
 
@@ -29,10 +30,8 @@ def index(request):
 
 
 def place_detail(request, place_id):
-    place = get_object_or_404(Place, id=place_id)
-
-    all_images = place.images.all().order_by('position')
-    image_urls = [img.image.url for img in all_images]
+    place = get_object_or_404(Place.objects.prefetch_related('images'), id=place_id)
+    image_urls = [img.image.url for img in place.images.all()]
 
     response_data = {
         "title": place.title,

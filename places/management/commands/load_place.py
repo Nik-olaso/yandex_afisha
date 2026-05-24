a34b922d6f9eeeaa2ff642dea33e2474fe66118c
Nik-olaso <122684717+Nik-olaso@users.noreply.github.com>
2026-05-24 11:22:41 +0300

    Избавился от комментариев

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,7 +1,7 @@
 import requests
 from django.core.management.base import BaseCommand
 from django.core.files.base import ContentFile
-from places.models import Place, Image # Твои названия моделей
+from places.models import Place, Image
 
 class Command(BaseCommand):
     help = 'Загружает данные о месте из JSON по URL'
